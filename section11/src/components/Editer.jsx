0e094ede3a451fc069596ae648c8a3c82b9d724f
주찬 <p990805@naver.com>
2025-01-14 16:25:40 +0900

    [주찬] Feat: Context 강의 시청 후 진행할 프로젝트 생성 01.14

---
+++ section11/src/components/Editer.jsx
@@ -0,0 +1,37 @@
+import './Editer.css'
+import { useState,useRef } from 'react'
+
+const Editer = ({onCreate}) => {
+    const [content, setContent] =useState("");
+    const contentRef = useRef();
+
+    const onChangeContent = (e) =>{
+        setContent(e.target.value);
+    }
+
+    const onKeydown = (e) => {
+        if(e.keyCode ===13){
+            onSubmit();
+        }
+    }
+
+    const onSubmit = ()=>{
+        if(content === ""){
+            contentRef.current.focus();
+            alert("빈 칸 입니다.")
+            return;
+        }
+        onCreate(content)
+        setContent("");
+    }
+    return <div className="Editer">
+        <input
+         ref={contentRef}
+         onKeyDown={onKeydown}
+         value={content}
+         onChange={onChangeContent} 
+         placeholder="새로운 Todo..."/>
+        <button onClick={onSubmit}>추가</button>
+    </div>
+}
+export default Editer;
---
+++ section11/src/components/List.jsx
@@ -0,0 +1,60 @@
+import './List.css'
+import TodoItem from './TodoItem';
+import {useState, useMemo} from "react"
+
+const List = ({todos,onUpdate, onDelete}) => {
+    const [search, setSearch] =useState("");
+
+    const onChangeSearch =(e) => {
+        setSearch(e.target.value);
+    };
+
+    const getFilteredData = () => {
+        if(search === ""){
+            return todos;
+        }
+        return todos.filter((todo)=>
+            todo.content.toLowerCase().includes(search.toLowerCase())
+        );
+    }
+
+    const filteredTodos = getFilteredData();
+
+    const {totalCount, doneCount, notDoneCount } = useMemo(()=>{
+        console.log("getAnalyzedData  호출출")
+        const totalCount = todos.length;
+        const doneCount =  todos.filter((todo)=>todo.isDone).length;
+        const notDoneCount = totalCount -doneCount;
+
+        return {
+            totalCount,
+            doneCount, 
+            notDoneCount
+        }
+    },[todos])
+    // 의존성 배열: deps
+    // useEffect도 deps에 들어간 값이 바뀌면 콜백 함수 실행
+    // 빈 deps로 하면 처음 랜더링 될때 한번만 실행된다.
+
+    //const {totalCount, doneCount, notDoneCount} = getAnalyzedData()
+
+    return <div className="List">
+        <h4>Todo List🌱</h4>
+        <div>
+            <div>total: {totalCount}</div>
+            <div>done: {doneCount}</div>
+            <div>notDone: {notDoneCount}</div>
+        </div>
+        <input 
+        value = {search}
+         onChange ={onChangeSearch}
+          placeholder="검색어를 입력하세요"
+           />
+        <div className="todos_wrapper">
+            {filteredTodos.map((todo) => {
+                return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete}/>;
+            })}
+        </div>
+    </div>
+}
+export default List;
