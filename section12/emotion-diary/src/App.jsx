fd99cbb8ea24d8cefb7d3e5bfc3a589cd08e5fba
박주찬 <p990805@naver.com>
2025-01-16 21:47:34 +0900

    [주찬] Feat : 동적 Router 실습 01.16

---
+++ section12/emotion-diary/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import Home from "./pages/Home"
+import Diary from "./pages/Diary"
+import New from "./pages/New"
+import NotFound from "./pages/NotFound"
+
+import {Routes, Route, Link, useNavigate } from "react-router-dom"
+
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 Nwe 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+
+function App() {
+  const nav =useNavigate();
+
+  const onClickbutton =() =>{
+    nav("/new");
+  }
+
+  return (
+    <>
+    <div>
+    <Link to ={"/"}>Home</Link>
+    <Link to ={"/new"}>New</Link>
+    <Link to ={"/diary"}>Diary</Link>
+    </div>
+    <button onClick={onClickbutton}>New 페이지로 이동</button>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/diary/:id" element={<Diary />} />
+      <Route path="/new" element={<New />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
