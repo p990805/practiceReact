23a2547c1ddf5cb209a65354ffb383e67946213b
박주찬 <p990805@naver.com>
2025-01-18 19:04:02 +0900

    [주찬] Feat: edit 페이지 생성/ 예시 mockData 생성 01.18

--- section12/emotion-diary/src/App.jsx
+++ section12/emotion-diary/src/App.jsx
@@ -3,56 +3,41 @@ import Home from "./pages/Home"
 import Diary from "./pages/Diary"
 import New from "./pages/New"
 import NotFound from "./pages/NotFound"
+import Edit from './pages/Edit'
 
-import Button from "./components/Button"
-import Header from "./components/Header"
+import { useReducer } from 'react'
 
-import {Routes, Route, Link, useNavigate } from "react-router-dom"
+import {Routes, Route } from "react-router-dom"
 
-import { getEmotionImage } from "./util/get-emotion-image";
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 Nwe 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId : 1,
+    content : "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId : 2,
+    content : "2번 일기 내용",
+  }
+]
+function reducer(state, action){
+  return state;
+}
 
 function App() {
-  const nav =useNavigate();
-
-  const onClickbutton =() =>{
-    nav("/new");
-  }
+  const [data,dispatch] = useReducer(reducer,mockData);
 
   return (
     <>
-    <Header title ={"Header"}
-      leftChild={<Button text={"left"} />}
-      rightChild={<Button text={"right"} />}
-    
-    />
-
-
-    <Button text={"123"} 
-    
-    onClick={()=>{
-      console.log("123번 버튼 클릭!")
-    }}/>
-
-    <Button text={"123"} 
-    type={"POSITIVE"}
-    onClick={()=>{
-      console.log("123번 버튼 클릭!")
-    }}/>
-
-    <Button text={"123"} 
-    type={"NEGATIVE"}
-    onClick={()=>{
-      console.log("123번 버튼 클릭!")
-    }}/>
-
-    
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/diary/:id" element={<Diary />} />
       <Route path="/new" element={<New />} />
+      <Route path="/edit/:id" element={<Edit />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
     </>
