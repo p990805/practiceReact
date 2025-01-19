2bda7ffa7e90c7441a88550e4ba26c62c0e35a95
박주찬 <p990805@naver.com>
2025-01-19 17:42:55 +0900

    [주찬] Feat : NEW.jsx 파일을 재구성하였습니다. 01.19

--- section12/emotion-diary/src/pages/New.jsx
+++ section12/emotion-diary/src/pages/New.jsx
@@ -1,4 +1,15 @@
+import Header from "../components/Header"
+import Button from "../components/Button"
+import Editor from "../components/Ediotr"
+
 const New =() => {
-    return <div>New</div>
+    return (
+        <div>
+            <Header title={"새 일기 쓰기"}
+                leftChild={<Button text={"< 뒤로 가기"}/>}
+            />
+            <Editor />
+        </div>
+    )
 }
 export default New
